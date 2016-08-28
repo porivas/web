e7e32d55f9a88c61a401eeb37a2852225885b43a
chenchen <ronalcc@126.com>
2016-08-29 07:20:42 +0800

    dd

---
+++ src/main/java/admin/dao/AddressdictMapper.java
@@ -0,0 +1,17 @@
+package admin.dao;
+
+import admin.model.Addressdict;
+import admin.model.AddressdictExample;
+import java.util.List;
+
+import common.core.generic.GenericDao;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.ResultMap;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.SelectKey;
+import org.apache.ibatis.annotations.Update;
+
+public interface AddressdictMapper extends GenericDao{
+}
